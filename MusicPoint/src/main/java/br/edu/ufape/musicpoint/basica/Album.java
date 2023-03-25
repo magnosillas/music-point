4d8266a64e4243775d17e10404edd03eee8dbe6a
Iasmin <iasminraquelsou@gmail.com>
2023-03-25 15:27:42 -0300

    Atributos classe basica album

--- MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Album.java
+++ MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Album.java
@@ -1,4 +1,52 @@
 package br.edu.ufape.musicpoint.basica;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String artist;
+    private String name;
+    private String releaseDate;
+    private String genre;
+
+    public Album() {
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
