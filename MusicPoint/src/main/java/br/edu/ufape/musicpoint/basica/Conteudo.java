cf4992aaf65e564f3d8b18f1ac8fec77e2b8f205
Magno Sillas <magno.sillas0@gmail.com>
2023-04-17 21:04:43 -0300

    pagina home em progresso

--- MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Conteudo.java
+++ MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Conteudo.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Conteudo {
+public class Conteudo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Review.java
+++ MusicPoint/src/main/java/br/edu/ufape/musicpoint/basica/Review.java
@@ -9,15 +9,41 @@ public class Review extends Postagem{
     private String titulo;
     @ManyToOne
     private Conteudo conteudo;
+    private String conteudoTipo;
+    private int nota;
 
     public Review() {
         super();
     }
 
-    public  Review(Usuario autor, String texto, String titulo, Conteudo conteudo){
+    public  Review(Usuario autor, String texto, String titulo, Conteudo conteudo, int nota){
         super(autor,texto);
         this.conteudo = conteudo;
+        if(conteudo instanceof Album){
+            this.conteudoTipo = "Álbum";
+        } else if (conteudo instanceof Musica) {
+            this.conteudoTipo = "Musica";
+        } else{
+            this.conteudoTipo = "Artista";
+        }
+        this.nota = nota;
         this.titulo=titulo;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getConteudoTipo() {
+        return conteudoTipo;
+    }
+
+    public void setConteudoTipo(String conteudoTipo) {
+        this.conteudoTipo = conteudoTipo;
     }
 
     public String getTitulo() {
